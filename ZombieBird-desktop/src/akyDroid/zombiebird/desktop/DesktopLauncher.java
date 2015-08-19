9bf543184e3e0012e77e763b6923b340b49610fb
Akshay Goel <aksha@GoelsPC>
2015-08-19 10:33:06 +0530

    committing andy pro

---
+++ ZombieBird-desktop/src/akyDroid/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package akyDroid.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import akyDroid.zombiebird.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
