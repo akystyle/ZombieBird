10f8a1c9d15c7badced4bd83b288c24dbdf705b0
Akshay Goel <akshay_goel@live.in>
2015-08-21 09:50:13 +0530

    finally installations done

--- ZombieBird-android/src/akyDroid/zombiebird/android/AndroidLauncher.java
+++ ZombieBird-android/src/akyDroid/zombiebird/android/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-
 import akyDroid.zombiebird.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -12,8 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
-
 		initialize(new MyGdxGame(), config);
 	}
 }
--- ZombieBird-desktop/src/akyDroid/zombiebird/desktop/DesktopLauncher.java
+++ ZombieBird-desktop/src/akyDroid/zombiebird/desktop/DesktopLauncher.java
@@ -7,8 +7,7 @@ import akyDroid.zombiebird.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
-		config.title = "akyDroid's Zombie Bird";
+		config.title = "AkyDroid's app";
 		config.height = 320;
 		config.width = 480;
 		new LwjglApplication(new MyGdxGame(), config);
