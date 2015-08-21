a99fd46c34dacfd9a9c889392e6cad4a056df828
Akshay Goel <akshay_goel@live.in>
2015-08-21 07:45:57 +0530

    uploading libgdx setup

---
+++ ZombieBird-android/gen/akyDroid/zombiebird/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package akyDroid.zombiebird.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- ZombieBird-desktop/src/akyDroid/zombiebird/desktop/DesktopLauncher.java
+++ ZombieBird-desktop/src/akyDroid/zombiebird/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import akyDroid.zombiebird.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+		config.title = "akyDroid's Zombie Bird";
+		config.height = 320;
+		config.width = 480;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
